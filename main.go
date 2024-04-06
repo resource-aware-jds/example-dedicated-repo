f996d72769d4ccd0ec920bd92d7480c19ce551da
Siratee K <siratee.k@lmwn.com>
2024-04-06 11:35:55 +0700

    fix: Update

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	containerlib "github.com/resource-aware-jds/container-lib"
 	"github.com/resource-aware-jds/container-lib/model"
 	"github.com/resource-aware-jds/container-lib/pkg/containerlibcontext"
 	"github.com/sirupsen/logrus"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -34,10 +38,37 @@ func main() {
 			unmarshalledData.MemoryAllocationSize = &size
 		}
 
-		fakeAllocation := make([]byte, *unmarshalledData.MemoryAllocationSize)
-		for i := 0; i < *unmarshalledData.MemoryAllocationSize; i++ {
-			fakeAllocation[i] = 0x99
-		}
+		innerCtx := context.Background()
+		newCtx, cancelFunc := context.WithCancel(innerCtx)
+		a := make([]byte, (*unmarshalledData.MemoryAllocationSize)*1000000)
+		fmt.Println("Reserved: ", " : ", *unmarshalledData.MemoryAllocationSize, "mb")
+		fmt.Println("Press Ctrl+C to abort this operation")
+		go func(innerCtx context.Context) {
+			for {
+				select {
+				case <-ctx.Done():
+					fmt.Println("Done.")
+					return
+				default:
+					for i := range a {
+						a[i] = 0x99
+					}
+				}
+				fmt.Println("Sleep 10 Second before reserving the next")
+				time.Sleep(10 * time.Second)
+			}
+
+		}(newCtx)
+
+		// Gracefully Shutdown
+		// Make channel listen for signals from OS
+		gracefulStop := make(chan os.Signal, 1)
+		signal.Notify(gracefulStop, syscall.SIGTERM)
+		signal.Notify(gracefulStop, syscall.SIGINT)
+
+		<-gracefulStop
+
+		cancelFunc()
 
 		time.Sleep(sleepTime)
 		fmt.Println(unmarshalledData)
